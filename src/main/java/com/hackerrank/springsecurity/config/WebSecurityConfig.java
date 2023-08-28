59b8fe9bed732a2c40ead52d9ce84ce74469733c
User <user@hackerrank.workspace>
2023-08-28 07:17:13 +0000

    Add initial repository
    Commit by HackerRank

---
+++ src/main/java/com/hackerrank/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.hackerrank.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("john_doe")
+                .password("{noop}student_password")
+                .authorities("ROLE_STUDENT_USER")
+                .and()
+                .withUser("jane_doe")
+                .password("{noop}admin_password")
+                .authorities("ROLE_OFFICE_ADMIN");
+    }
+}
